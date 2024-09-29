f7f493b8d8813dc67377e212dfabb97368a3b716
Arbab Hussain <125045872+ArbabHussainQ@users.noreply.github.com>
2024-09-29 20:04:54 +0500

    Create 3. operations.js

---
+++ 2. Datatypes/3. operations.js
@@ -0,0 +1,28 @@
+
+// SOME COMMON OPERATIONS IN JS
+
+let value = 28;
+let negValue = -value;
+console.log(negValue); // -28
+
+console.log(3+3); // 6
+console.log(3-3); // 0
+console.log(3*3); // 9
+console.log(3/3); // 1
+console.log(3%2); // 1
+console.log(3**2);  // 3**2 => 3^2 => 9
+console.log(5**3);  //  5**3 =>5^3 => 125
+
+let str1 = "Arbab";
+let str2 = "Hussain";
+let str3 = str1 + " " + str2;
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log(1 + 2); // 3
+console.log("1" + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log("1" + (2 + 2)); // 14
+console.log(1 + 2 + "2"); // 32
+console.log( (1 + 2) * 8 / (4 / 2)); // 12
